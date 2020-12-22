de7be3add2f6c82d62b160e4401762974ac0640d
ZhdankovAlex <47736968+ZhdankovAlex@users.noreply.github.com>
2020-12-22 13:54:49 +0300

    Добавьте файлы проекта.

---
+++ EquidistantSubsets/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace EquidistantSubsets
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(13, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(900, 900);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(919, 40);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(226, 795);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(991, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Points:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(920, 842);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(225, 70);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Go";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1155, 924);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ EquidistantSubsets/Form1.cs
@@ -0,0 +1,240 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+/* TEST:
+
+
+-27
+-15
+-9
+-9
+-15
+-33
+12
+12
+6
+27
+27
+21
+-9
+-21
+27
+6
+
+
+*/
+
+namespace EquidistantSubsets
+{
+    public partial class Form1 : Form
+    {
+        Graphics drawArea;
+        SolidBrush brush;
+        Pen pen;
+
+        Point Center;
+        Point Mid;
+        Point Right; /* == */ Point Point2;
+        Point Left; /* == Point1*/
+        Point Point3;
+        int Ymin;
+        int Ymax;
+
+        //массив всех точек
+        List<Point> allPoints = new List<Point>() { };
+        
+        public Form1()
+        {
+            InitializeComponent();
+            drawArea = pictureBox1.CreateGraphics();
+        }
+
+        public void DrawGrid()
+        {
+            brush = new SolidBrush(Color.Gray);
+            drawArea.FillRectangle(brush, 0, 0, pictureBox1.Width, pictureBox1.Height);
+
+            //нарисуем координатную сетку
+            pen = new Pen(Color.Black);
+
+            //вертикальные
+            for (int i = 0; i < pictureBox1.Width; i += 5)
+            {
+                drawArea.DrawLine(pen, i, 0, i, pictureBox1.Height);
+            }
+
+            //горизонтальные
+            for (int i = 0; i < pictureBox1.Height; i += 5)
+            {
+                drawArea.DrawLine(pen, 0, i, pictureBox1.Width, i);
+            }
+
+            //деление на четверти
+            pen = new Pen(Color.Red, 2);
+            drawArea.DrawLine(pen, Center.X, 0, Center.X, pictureBox1.Height);
+            drawArea.DrawLine(pen, 0, Center.Y, pictureBox1.Width, Center.Y);
+        }
+
+        public void DrawPoints()
+        {
+            //отметим точки
+            pen = new Pen(Color.Orange, 2);
+            string[] lines_points = textBox1.Lines;
+            Point currentPoint;
+            for (int i = 0; i < lines_points.Length; i += 2)
+            {
+                currentPoint = new Point(int.Parse(lines_points[i]), int.Parse(lines_points[i + 1]));
+                allPoints.Add(currentPoint);
+                drawArea.DrawEllipse(pen, Center.X + currentPoint.X * 5 - 1, Center.Y - currentPoint.Y * 5 - 1, 2, 2);
+
+                if (Ymin > currentPoint.Y)
+                    Ymin = currentPoint.Y;
+
+                if (Ymax < currentPoint.Y)
+                    Ymax = currentPoint.Y;
+            }
+
+            //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!отладка!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+            Console.WriteLine("Ymin: " + Ymin);
+            Console.WriteLine("Ymax: " + Ymax);
+        }
+
+        public void SortPoints()
+        {
+            //упорядочиваем список точек по возрастанию Х (если Х совпадает, то по У)
+            allPoints = allPoints.OrderBy(p => p.X).ThenBy(p => p.Y).ToList();
+
+            //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!отладка!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+            Console.WriteLine("All Points:");
+            for (int i = 0; i < allPoints.Count(); i++)
+            {
+                Console.WriteLine(allPoints[i].X + "; " + allPoints[i].Y);
+            }
+        }
+
+        public void FindMid()
+        {
+            //находим среднюю точку
+            Mid = allPoints[allPoints.Count() / 2];
+
+            //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!отладка!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+            Console.WriteLine("Mid: " + Mid);
+        }
+
+        public void FindLeft()
+        {
+            Point currentPoint;
+            //находим X1
+            currentPoint = Mid;
+            int space = 1;
+            while (currentPoint.X == Mid.X && allPoints.Count() / 2 >= space)
+            {
+                currentPoint = allPoints[allPoints.Count() / 2 - space];
+                space++;
+            }
+            Left = currentPoint;
+            //если пробежали весь список до начала
+            if (allPoints.Count() / 2 - space < 0)
+                Left = new Point(Mid.X - 1, Mid.Y - 1);
+
+            //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!отладка!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+            Console.WriteLine("Left == Point1: " + Left);
+        }
+
+        public void FindRight()
+        {
+            Point currentPoint;
+            //находим X2
+            currentPoint = Mid;
+            int space = 1;
+            while (currentPoint.X == Mid.X && allPoints.Count() / 2 + space < allPoints.Count())
+            {
+                currentPoint = allPoints[allPoints.Count() / 2 + space];
+                space++;
+            }
+            Right = currentPoint;
+            //если пробежали весь список до конца
+            if (allPoints.Count() / 2 + space == allPoints.Count())
+                Right = new Point(Mid.X + 1, Mid.Y + 1);
+
+            //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!отладка!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+            Console.WriteLine("Right: " + Right);
+        }
+
+        public void FindSegment()
+        {
+            //вычисляем последние две точки
+            int Z = Math.Min(Mid.X - Left.X, Right.X - Mid.X);
+            /* Если количество точек нечетно,
+             * то искомая линия проходит через среднюю точку,
+             * иначе над средней точкой */
+            if (allPoints.Count() / 2 == 0)
+            {
+                if ((Mid.Y + Left.Y) % 2 == 0)
+                    Point2 = new Point(Mid.X, (Mid.Y + Left.Y) / 2);
+                else
+                    Point2 = new Point(Mid.X, (Mid.Y + Left.Y) / 2 + 1);
+                if (Z % 2 == 0)
+                    Point3 = new Point(Mid.X + Z / 2, Mid.Y - Ymax + Ymin);
+                else
+                    Point3 = new Point(Mid.X + Z / 2 + 1, Mid.Y - Ymax + Ymin);
+            }
+            else
+            {
+                Point2 = new Point(Mid.X, Mid.Y);
+                if (Z % 2 == 0)
+                    Point3 = new Point(Mid.X + Z / 2, Mid.Y - Ymax + Ymin);
+                else
+                    Point3 = new Point(Mid.X + Z / 2 + 1, Mid.Y - Ymax + Ymin);
+            }
+
+            //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!отладка!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+            Console.WriteLine("Point2: " + Point2);
+            Console.WriteLine("Point3: " + Point3);
+        }
+
+        public void DrawSegment()
+        {
+            //рисуем нашу прямую
+            pen = new Pen(Color.LimeGreen, 2);
+            drawArea.DrawLine(pen, Center.X + Point2.X * 5, Center.Y - Point2.Y * 5, Center.X + Point3.X * 5, Center.Y - Point3.Y * 5);
+        }
+
+        public void Initialize()
+        {
+            Center = new Point(pictureBox1.Width / 2, pictureBox1.Height / 2);
+            Ymin = pictureBox1.Height;
+            Ymax = 0;
+            allPoints = new List<Point>() { };
+
+            DrawGrid();
+
+            DrawPoints();                        
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Initialize();
+
+            SortPoints();
+
+            FindMid();
+
+            FindLeft();
+
+            FindRight();
+
+            FindSegment();
+
+            DrawSegment();
+        }
+    }
+}
